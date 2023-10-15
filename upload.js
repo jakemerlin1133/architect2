4d1e73043fe03ca33e5910dfa37e95e8e48c7a97
jakemerlin <jakemerlin123@gmail.com>
2023-10-15 12:15:06 +0800

    code added

--- upload.js
+++ upload.js
@@ -1,25 +1,61 @@
-document.querySelector("#files").addEventListener("change", (e) =>{
-    if(window.File && window.FileReader && window.FileList && window.Blob){
-  
-      const files = e.target.files;
-      const output = document.querySelector("#result"); 
+let queuedImagesArray = [],
+  savedForm = document.querySelector("#saved-form"),
+  queuedForm = document.querySelector("#queued-form"),
+  savedDiv = document.querySelector(".saved-div"),
+  queuedDiv = document.querySelector(".queued-div"),
+  inputDiv = document.querySelector(".input-div"),
+  input = document.querySelector(".input-div input"),
+  serverMessage = document.querySelector(".server-message"),
+  deleteImages = [];
 
-        for(let i = 0; i < files.length; i++){
-            if(!files[i].type.match("image")) continue;
-            const picReader = new FileReader();
-            picReader.addEventListener("load", function(event){
-                const picFile = event.target;
-                const div = document.createElement("div");
-                div.innerHTML = `<img class="thumbnail" src="${picFile.result}" title="${picFile.name}"/>`;
-                output.appendChild(div);
-            })
+// QUEUED IN FRONTEND IMAGES
+input.addEventListener("change", () => {
+  const files = input.files;
+  for (let i = 0; i < files.length; i++) {
+    queuedImagesArray.push(files[i]);
+  }
+  queuedForm.reset();
+  displayQueuedImages();
+});
 
-            picReader.readAsDataURL(files[i])
-            
-        }
+inputDiv.addEventListener("drop", (e) => {
+  e.preventDefault();
+  const files = e.dataTransfer.files;
+  for (let i = 0; i < files.length; i++) {
+    if (!files[i].type.match("image")) continue;
 
-    }else{
-      alert("Your browser does not support the File API")
-    }
-  
+    if (queuedImagesArray.every((image) => image.name !== files[i].name))
+      queuedImagesArray.push(files[i]);
+  }
+  displayQueuedImages();
+});
+
+function displayQueuedImages() {
+  let images = "";
+  queuedImagesArray.forEach((image, index) => {
+    images += `<div class="image">
+                 <img src="${URL.createObjectURL(image)}" alt="image">
+                   <span onclick="deleteQueuedImage(${index})">&times;</span>
+              </div>`;
   })
+  queuedDiv.innerHTML = images;
+}
+
+function deleteQueuedImage(index){
+    queuedImagesArray.splice(index, 1)
+    displayQueuedImages()
+}
+
+queuedForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    sendQueuedImagesToServer()
+})
+
+function sendQueuedImagesToServer(){
+  const formData = new FormData(queuedForm)
+
+  queuedImagesArray.forEach((image, index) => {
+      formData.append(`file[${index}]`, image)
+  })
+
+}
--- upload.php
+++ upload.php
@@ -8,23 +8,61 @@
     <script src="https://kit.fontawesome.com/445aa1d2b6.js" crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="upload.css">
     <title>Document</title>
 </head>
 
 <body>
 
-<label for="files">Select multiple files</label>
-    <input type="file" id="files" multiple="multiple" accept="image/jpeg, image/png, image/png, image/webp">
-    <output id="result"></output>
+    <div class="text-center">
+        <img class="max-[320px]:ml-[80px] min-[320px]:mx-auto mt-[2rem] rounded-lg" height="150" width="150" src="Architect_files/images/sample-logo.png">
+        <div class="font-bold text-3xl mt-[1rem]">SZD Architectural Design Studio</div>
+    </div>
 
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
-    
+    <main class="app">
+        <form id="queued-form">
+
+            <div>
+                <h1>Title of the Design</h1>
+                <input class="w-full focus:outline-none" type="text" placeholder="Title">
+            </div>
+
+
+            <div class="mt-[2rem]">
+                <h1>Desciption of the Design</h1>
+                <textarea class="w-full h-[50vh] focus:outline-none p-[10px]" placeholder="Description"></textarea>
+            </div>
+
+            <div class="header">
+                <h2>Upload Image </h2>
+                <div class="server-message"></div>
+            </div>
+            <div class="input-div">
+                <p>Drag & Drop images here <span class="browse">Browser</span></p>
+                <input type="file" class="file" multiple="multiple" accept="image/png, image/jpeg, image/jpg, image/webp">
+            </div>
+
+            <div class="queued-div"></div>
+            <div class="text-center mt-[1rem]">
+                <button class="mx-auto" type="submit">Upload</button>
+            </div>
+        </form>
+    </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <script src="upload.js"></script>
-    <script>
-        AOS.init();
-    </script>
 </body>
 
 </html>
