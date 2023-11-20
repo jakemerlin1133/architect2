721ce68c2402bd02f4a2937423898fad2b2c86ea
jakemerlin <jakemerlin123@gmail.com>
2023-11-20 19:05:20 +0800

    code changed

--- category.js
+++ category.js
@@ -3,98 +3,90 @@ const container = document.querySelector("#container");
 const showcaseItems = container.querySelectorAll("#showcase");
 
 categories.addEventListener("change", () => {
-  let totalShowcaseForCategory = 0;
-  const categoriesValue = categories.value.toUpperCase();
 
+  const categoriesValue = categories.value.toUpperCase();
   for (let i = 0; i < showcaseItems.length; i++) {
     const categoryValue = showcaseItems[i].querySelector("h2");
     if (categoriesValue === "ALL") {
       showcaseItems[i].style.display = "block";
-      totalShowcaseForCategory++;
     } else {
       if (categoryValue.innerHTML.toUpperCase().indexOf(categoriesValue) > -1) {
         showcaseItems[i].style.display = "block";
-        totalShowcaseForCategory++;
       } else {
         showcaseItems[i].style.display = "none";
       }
     }
   }
   
-  let limit = totalShowcaseForCategory;
-  loadShowcase(limit)
 });
 
 // pagination of showcase
 
-// let limit = 3;
-
-function loadShowcase(limit){
-  let page = 1;
-console.log(limit);
+let page = 1;
+let limit = 6;
+function loadShowcase(){
+  let begin = limit * (page - 1);
+  let end = limit * page - 1;
+  showcaseItems.forEach((showcase, index) => {
+    if(index >= begin && index <= end){
+      showcase.style.display = "block";
+    }else{
+      showcase.style.display = "none";
+    }
+  });
+  listPage();
 }
-//   let begin = limit * (page - 1);
-//   let end = limit * page - 1;
-//   showcaseItems.forEach((showcase, index) => {
-//     if(index >= begin && index <= end){
-//       showcase.style.display = "block";
-//     }else{
-//       showcase.style.display = "none";
-//     }
-//   });
-//   listPage();
-// }
-// loadShowcase();
+loadShowcase();
 
-// function listPage(){
-//   let count = Math.ceil(showcaseItems.length / limit);
-//   document.querySelector("#pagination").innerHTML = "";
-//   let pagination = document.querySelector("#pagination");
+function listPage(){
+  let count = Math.ceil(showcaseItems.length / limit);
+  document.querySelector("#pagination").innerHTML = "";
+  let pagination = document.querySelector("#pagination");
 
 
-//   let GotoStart = document.createElement("li");
-//   GotoStart.innerText = "<<";
-//   GotoStart.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
-//   GotoStart.setAttribute("onclick", "changePage(1)");
-//   pagination.appendChild(GotoStart);
+  let GotoStart = document.createElement("li");
+  GotoStart.innerText = "<<";
+  GotoStart.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
+  GotoStart.setAttribute("onclick", "changePage(1)");
+  pagination.appendChild(GotoStart);
 
-//   let prev = document.createElement("li");
-//   prev.innerText = "<";
-//   prev.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
-//   if(page > 1){
-//     prev.setAttribute("onclick", "changePage(" + (page - 1 ) + ")");
-//   }  
-//   pagination.appendChild(prev);
+  let prev = document.createElement("li");
+  prev.innerText = "<";
+  prev.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
+  if(page > 1){
+    prev.setAttribute("onclick", "changePage(" + (page - 1 ) + ")");
+  }  
+  pagination.appendChild(prev);
 
-//   for(let i = 1; i <= count; i++){
-//     let newPage = document.createElement("li");
-//     newPage.innerText = i;
-//     newPage.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
-//     if(i === page){
-//       newPage.classList.add("active");
-//     }
-//     newPage.setAttribute("onclick", "changePage(" + i + ")");
-//     pagination.appendChild(newPage);
-//   }
+  for(let i = 1; i <= count; i++){
+    let newPage = document.createElement("li");
+    newPage.innerText = i;
+    newPage.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
+    if(i === page){
+      newPage.classList.add("active");
+    }
+    newPage.setAttribute("onclick", "changePage(" + i + ")");
+    pagination.appendChild(newPage);
+  }
 
-//   let next = document.createElement("li");
-//   next.innerText = ">";
-//   next.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
-//   if(page < count){
-//     next.setAttribute("onclick", "changePage(" + (page + 1 ) + ")");
-//   }
+  let next = document.createElement("li");
+  next.innerText = ">";
+  next.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
+  if(page < count){
+    next.setAttribute("onclick", "changePage(" + (page + 1 ) + ")");
+  }
 
-//   pagination.appendChild(next);
+  pagination.appendChild(next);
 
-//   let goToEnd = document.createElement("li");
-//   goToEnd.innerText = ">>";
-//   goToEnd.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
-//   goToEnd.setAttribute("onclick", "changePage(" + count + ")");
-//   pagination.appendChild(goToEnd);
-// }
+  let goToEnd = document.createElement("li");
+  goToEnd.innerText = ">>";
+  goToEnd.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
+  goToEnd.setAttribute("onclick", "changePage(" + count + ")");
+  pagination.appendChild(goToEnd);
+}
 
 
-// function changePage(i){
-//   page = i;
-//   loadShowcase();
-// }
+function changePage(i){
+  page = i;
+  loadShowcase();
+}
