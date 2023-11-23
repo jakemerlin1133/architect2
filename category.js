f9ef9ad32188d59aff762855aed49d1c31d168ba
jakemerlin1133 <jakemerlin123@gmail.com>
2023-11-23 13:50:58 +0800

    code added

--- category.js
+++ category.js
@@ -3,72 +3,91 @@ const pageUI = document.querySelector("#pagination");
 const categories = document.querySelector("#categories");
 const showcase = container.querySelectorAll("#showcase");
 let page = 1;
+let limit = 5;
 
-showcase.forEach((item, index) => {
-  if (index >= 0 && index <= 5) {
-    item.style.display = "block";
-  } else {
-    item.style.display = "none";
-  }
-});
-
-function pages(){
-categories.addEventListener("change", () => {
-  const categoriesValue = categories.value.toUpperCase();
-  let limit = 6;
-
-  let begin = limit * (page - 1);
+changePage(page);
+function loadCount(){
+  let start = limit * (page - 1);
   let end = limit * page - 1;
-  let totalItem = 0;
-
-  showcase.forEach((showcase) => {
-    const h2 = showcase.querySelector("h2").innerText.toUpperCase();
-    if (categoriesValue === h2 || categoriesValue === "ALL") {
-      totalItem++;
-      if ((totalItem -1) >= begin && (totalItem -1) <= end) {
-        showcase.style.display = "block";
-      } else {
-        showcase.style.display = "none";
-      }
+  showcase.forEach((item, index) => {
+    if (index >= start && index <= end) {
+      item.style.display = "block";
     } else {
-      showcase.style.display = "none";
+      item.style.display = "none";
     }
   });
-
-  
-  loadCount(totalItem, limit);
-});
+  listPage(showcase);
 }
+loadCount();
 
+function listPage(listofShowCase){
+  pageUI.innerHTML = "";
+  let count = document = Math.ceil(listofShowCase.length / limit);
 
-function loadCount(totalcount, limit){
-  showcasePerPage = Number(totalcount);
-  listPage(showcasePerPage, limit);
+  let goToStart = document.createElement('li');
+  goToStart.innerHTML = "<<";
+  goToStart.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800", "cursor-pointer");
+  goToStart.setAttribute("onclick","changePage(1)");
+  pageUI.appendChild(goToStart);
 
-}
-// // // pagination of showcase
-function listPage(totalItem, limit){
-  document.querySelector("#pagination").innerHTML = "";
-  let pagination = document.querySelector("#pagination");
-  let count = Math.ceil( totalItem / limit);
-  pageCount(count);
-}
+  let prev = document.createElement('li');
+  prev.innerHTML = "<";
+  prev.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800", "cursor-pointer");
+  if(page > 1){
+    prev.setAttribute("onclick","changePage(" + (page - 1) +")");
+  }
+  pageUI.appendChild(prev);
 
-function pageCount (totalPage){
-
-  for(let i = 1; i <= totalPage; i++){
+  for(let i =1; i <= count; i++){
     let newPage = document.createElement('li');
-    newPage.innerText = i;
-    newPage.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800");
+    newPage.innerHTML = i;
+    if(i === page){
+      newPage.classList.add("active");
+    }
+    newPage.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800", "cursor-pointer");
     newPage.setAttribute("onclick","changePage(" + i +")");
-    pagination.appendChild(newPage);
-}
+    pageUI.appendChild(newPage);
+  }
+
+  let next = document.createElement('li');
+  next.innerHTML = ">";
+  next.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800", "cursor-pointer");
+  if(page < count){
+    next.setAttribute("onclick","changePage(" + (page + 1) +")");
+  }
+  pageUI.appendChild(next);
+
+  let goToEnd = document.createElement('li');
+  goToEnd.innerHTML = ">>";
+  goToEnd.classList.add("font-normal", "text-gray-400", "border-[2px]", "border-gray-300", "px-6", "py-4", "mx-1", "shadow-md", "hover:text-white", "hover:bg-slate-800", "cursor-pointer");
+  goToEnd.setAttribute("onclick","changePage(" + count + ")");
+  pageUI.appendChild(goToEnd);
 }
 
-function changePage(newPage){
-page = newPage;
-  pages();
-  console.log(page);
+function changePage(pageNumber){
+  page = pageNumber;
+  loadCount();
 }
-pages();
+
+function selectCategory(){ 
+  categories.addEventListener("change", () => {
+    pageUI.innerHTML = "";
+    let totalItem = 0;
+    const categoriesValue = categories.value.toLocaleUpperCase();
+
+    showcase.forEach((showcase) =>{
+      const h2 = showcase.querySelector("h2").innerText.toLocaleUpperCase();
+
+      if(categoriesValue === h2 || categoriesValue === "ALL"){
+        totalItem++;
+        showcase.style.display = "block";
+      }else{
+        showcase.style.display = "none";
+      }
+    });
+
+    // changePage(1);
+  });
+}
+selectCategory();
 
